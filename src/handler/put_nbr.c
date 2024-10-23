5b7fa6578d480ca79f1edff18a8b112a3c70c30a
Marc Agen <marc.agen@epitech.eu>
2024-10-23 16:48:40 +0200

    fix: fix the put_nbr.c

--- src/handler/put_nbr.c
+++ src/handler/put_nbr.c
@@ -4,19 +4,30 @@
 ** File description:
 ** display the number given as parameter
 */
+#include<unistd.h>
 #include "printf.h"
+
+static void baby_putchar(char c)
+{
+    write(1, &c, 1);
+}
 
 int printf_put_nbr(int nb)
 {
     if (nb < 0) {
-        write(1, '-', 1);
+        baby_putchar('-');
         nb *= -1;
     }
     if (nb < 10) {
-        write(1, nb + '0', 1);
+        baby_putchar(nb + '0');
     } else {
         printf_put_nbr(nb / 10);
-        write(1, nb % 10 +'0', 1);
+        baby_putchar(nb % 10 +'0');
     }
     return 0;
+}
+
+int main(void)
+{
+    printf_put_nbr(42);
 }
