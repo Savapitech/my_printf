c290645aae238e0bb92c46f4c2e103b31a66df65
Marc Agen <marc.agen@epitech.eu>
2024-10-24 11:35:22 +0200

    jaurais pas du

---
+++ src/handler/put_pointer.c
@@ -0,0 +1,10 @@
+/*
+** EPITECH PROJECT, 2024
+** put_pointer
+** File description:
+** display the adress given as parameter
+** in the hexadecimal format
+*/
+#include "printf.h"
+
+int printf_put_pointer(va_list )
