e7d582f75663c47c3f1bc18e79d36a11a8385bbe
Marc Agen <marc.agen@epitech.eu>
2024-10-27 04:29:55 +0100

    fix: fix all warning

--- src/handler/put_nbr.c
+++ src/handler/put_nbr.c
@@ -7,24 +7,6 @@
 
 #include "my.h"
 
-static
-int baby_put_nbr(int nb)
-{
-    int count = 0;
-
-    if (nb < 0) {
-        count += baby_putchar('-');
-        nb *= -1;
-    }
-    if (nb < 10) {
-        count += baby_putchar(nb + '0');
-    } else {
-        count += baby_put_nbr(nb / 10);
-        count += baby_putchar(nb % 10 +'0');
-    }
-    return count;
-}
-
 int printf_put_nbr(flags_t *flags)
 {
     return baby_put_nbr(va_arg(flags->args, int));
